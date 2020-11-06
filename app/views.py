54ad0044e1683d167c08d0dcd8158139d69e8f11
Iman Ashoori <56173335+imanashoorii@users.noreply.github.com>
2020-11-06 11:55:02 +0330

    init

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'base.html')
